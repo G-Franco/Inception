d3e1cc94f83ec2a1599ff857d2e69d31612bdff7
Gabriel Franco <gguerrafranco@gmail.com>
2024-01-18 16:13:29 +0000

    wordpress is better but need mariadb operational to test

--- srcs/requirements/wordpress/conf/wordpress_conf.sh
+++ srcs/requirements/wordpress/conf/wordpress_conf.sh
@@ -4,7 +4,7 @@ echo "[Wordpress startup] Waiting for MariaDB..."
 # -u - user
 # -p - password
 # dev/null - redirect output to /dev/null to silence output
-while ! mariadb -h${MDB_HOST} -u${MDB_USER} -p${MDB_ADMIN_PASSWORD} ${MDB_NAME} &>/dev/null;
+while ! mysql -h${MDB_HOST} -u${MDB_USER} -p${MDB_ADMIN_PASSWORD} ${MDB_NAME} &>/dev/null;
 do
     sleep 3
 done
@@ -17,19 +17,19 @@ then
 else
 	echo "[Wordpress startup] Setting up WordPress"
 	echo "[Wordpress startup] Updating WP-CLI tool"
-	wp-cli.phar cli update --yes --allow-root
+	wp cli update --yes --allow-root
 	echo "[Wordpress startup] Downloading WordPress"
-	wp-cli.phar core download --allow-root
+	wp core download --allow-root
 	echo "[Wordpress startup] Creating wp-config.php"
-	wp-cli.phar config create --dbname=${MDB_NAME} --dbuser=${MDB_USER} --dbpass=${WP_USER_PASSWORD} --dbhost=${MDB_HOST} --path=${WP_PATH} --allow-root
+	wp config create --dbname=${MDB_NAME} --dbuser=${WP_ADMIN_USER} --dbpass=${WP_ADMIN_PASSWORD} --dbhost=${MDB_HOST} --path=${WP_PATH} --allow-root
 	echo "[Wordpress startup] Installing WordPress core"
-	wp-cli.phar core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_PATH} --allow-root
+	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_PATH} --allow-root
 	echo "[Wordpress startup] Creating WordPress default user"
-	wp-cli.phar user create ${MDB_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --display_name=${MDB_USER} --porcelain --path=${WP_PATH} --allow-root
+	wp user create ${MDB_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --display_name=${MDB_USER} --porcelain --path=${WP_PATH} --allow-root
 	echo "[Wordpress startup] Installing WordPress theme"
-	wp-cli.phar theme install bravada --path=${WP_PATH} --activate --allow-root
-	wp-cli.phar theme status bravada --allow-root
+	wp theme install bravada --path=${WP_PATH} --activate --allow-root
+	wp theme status bravada --allow-root
 fi
 
 echo "[Wordpress startup] Starting WordPress fastCGI on port 9000."
-exec /usr/sbin/php-fpm81 -F -R
+exec /usr/sbin/php-fpm7.4 -F -R
