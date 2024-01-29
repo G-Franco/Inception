a4629681f60276960eeca09d6b2b3b8d8777937e
Gabriel Franco <gguerrafranco@gmail.com>
2024-01-29 12:42:43 +0000

    testing stuff

--- srcs/requirements/mariadb/conf/mdb_conf.sh
+++ srcs/requirements/mariadb/conf/mdb_conf.sh
@@ -7,18 +7,16 @@ if [ ! -d "/run/mysqld" ]; then
 fi
 
 # Check for existing database
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-	# Temporary file stores SQL commands for bootstrap installation of MariaDB
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
-
- 	# Write SQL commands to temporary file
-	cat << EOF > $tfile
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+chown -R mysql:mysql /var/lib/mysql
+mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+# Temporary file stores SQL commands for bootstrap installation of MariaDB
+tfile=$(mktemp)
+if [ ! -f "$tfile" ]; then
+	return 1
+fi
+# Write SQL commands to temporary file
+cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
 
@@ -35,13 +33,13 @@ GRANT ALL PRIVILEGES ON $MDB_NAME.* TO '$MDB_USER'@'%';
 
 FLUSH PRIVILEGES;
 EOF
-	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-	rm -f $tfile
-fi
+/usr/sbin/mysqld --user=mysql --bootstrap < $tfile
+rm -f $tfile
+# fi
 
 # Allow remote connections to the database
 # sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Finally, it starts the MariaDB server in console mode, using the 'mysql' user.
-exec /usr/bin/mysqld --user=mysql --console
+exec /usr/sbin/mysqld --user=mysql --console
